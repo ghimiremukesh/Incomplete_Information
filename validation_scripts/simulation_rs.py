f9da110fff70b6c21df7267d2af29fed2bc754a6
ghimiremukesh <ghimiremukesh1@gmail.com>
2022-08-11 15:07:26 -0400

    fix splitting point bugs

--- validation_scripts/simulation_rs.py
+++ validation_scripts/simulation_rs.py
@@ -89,7 +89,7 @@ def hji_compute(X_nn, t_nn, model):
 
     # unnormalize the costate for agent 1
     lam_d = dvdx[:, :, :1].squeeze()
-    lam_v = dvdx[:, :, 1:2].squeeze()
+    lam_v = dvdx[:, :, -1:].squeeze()
 
     u_c = torch.tensor([-0.3, 0.3]).to(device)
     d_c = torch.tensor([-0.1, 0.1]).to(device)
@@ -121,11 +121,46 @@ def dynamic(X_nn, dt, action):
     return d, v
 
 
-def optimization(X_nn, t_nn, dt, model):
+def optimization(X_nn, t_nn, dt, model, type):
     # \sum lambda_j * p_j = p
     def constraint(var):
         constrain = var[0] * var[1] + (1 - var[0]) * var[2] - X_nn[-1]
         return abs(constrain) <= 5e-3
+
+    def objective_v(var):
+        lam_1 = var[0]
+        lam_2 = 1 - var[0]
+        p1 = var[1]
+        p2 = var[2]
+
+        _, _, V = value_action(X_nn, t_nn, model)
+
+        # 1. obtain action for splitting point 1 and 2
+        # 2. compute value for splitting point 1 and 2
+        # splitting point 1
+        X_nn1 = copy.deepcopy(X_nn)
+        X_nn1[-1] = p1
+        u1_s, u2_s, _ = value_action(X_nn1, t_nn, model)
+        d1, v1 = dynamic(X_nn1, dt, (u1_s, u2_s))
+
+        X_nn1 = np.vstack((d1, v1, p1))
+        t_nn1 = t_nn - dt
+        _, _, V1 = value_action(X_nn1, t_nn1, model)
+
+        # splitting point 2
+        X_nn2 = copy.deepcopy(X_nn)
+        X_nn2[-1] = p2
+        u1_s, u2_s, _ = value_action(X_nn2, t_nn, model)
+        d2, v2 = dynamic(X_nn1, dt, (u1_s, u2_s))
+
+        X_nn2 = np.vstack((d2, v2, p2))
+        t_nn2 = t_nn - dt
+        _, _, V2 = value_action(X_nn2, t_nn2, model)
+
+        # loss = V(t_k) - \sum lambda_i * V(t_k+1, p_i)
+        loss = V - (lam_1 * V1 + lam_2 * V2)
+
+        return loss
 
     def objective(var):
         lam_1 = var[0]
@@ -153,12 +188,17 @@ def optimization(X_nn, t_nn, dt, model):
 
         return loss
 
-    search_space = np.linspace(0, 1, num=50)
+    search_space = np.linspace(0, 1, num=11)
     # 1-D grid search for lambda, p1, p2
     grid = product(search_space, repeat=3)  # make a grid
     reduced = filter(constraint, grid)  # apply filter to reduce the space
-    opt_x = min(reduced, key=objective)  # find 3-uple corresponding to min objective func.
+    opt_x = min(reduced, key=objective_v)  # find 3-uple corresponding to min objective func.
+
     p = X_nn[-1, :]
+
+    if type == 1:  # p_i corresponds to which type you are
+        p = 1 - p
+
     if p == 0:
         p = p + 1e-2
 
@@ -195,7 +235,8 @@ if __name__ == '__main__':
     logging_root = './logs'
 
     # Setting to plot
-    ckpt_path = '../logs/relative_0.0/checkpoints/model_final.pth'
+    ckpt_path = '../logs/p_from_coords/checkpoints/model_final.pth'
+    # ckpt_path = '../experiment_scripts/logs/4d_picnn_min_hji/checkpoints/model_final.pth'
     activation = 'tanh'
 
     # Initialize and load the model
@@ -214,62 +255,93 @@ if __name__ == '__main__':
     model.load_state_dict(model_weights)
     model.eval()
 
-    num_physical = 2
-    x0 = torch.zeros(1, num_physical).uniform_(-1, 1)
-    x0[:, 0] = 0  # put them in the center
-    x0[:, 1] = 0
+    num_games = 10
+    # p_dist = [0.6, 0.4]
+    # types = [0, 1]
+    # types_i = np.random.choice(types, size=num_games, p=p_dist) # nature selection from dist
+    # p_0 = p_dist[0]# types_i = np.zeros(num_games) # random selection
+    for i in range(num_games):
+        num_physical = 2
+        x0 = torch.zeros(1, num_physical).uniform_(-1, 1)
+        x0[:, 0] = 0.1 # put them in the center
+        x0[:, 1] = 0
 
-    p = np.array([[0.0]])
-    X0 = np.vstack((x0.T, p))
+        # probability selections and calculations
+        p_dist = np.random.rand()
+        p_dist = [p_dist, 1 - p_dist]
+        types = [0, 1]
+        type_i = np.random.choice(types, p=p_dist)  # nature selection from dist
+        p_0 = p_dist[0]  # types_i = np.zeros(num_games) # random selection
 
-    N = 50
-    Time = np.linspace(0, 1, num=N)
-    dt = Time[1] - Time[0]
-    Time = np.flip(Time)
 
-    d = np.zeros((N,))
-    v = np.zeros((N,))
-    u1 = np.zeros((N,))
-    u2 = np.zeros((N,))
-    p = np.zeros((N,))
-    V = np.zeros((N,))
+        X0 = np.vstack((x0.T, p_0))
 
-    d[0] = X0[0, :]
-    v[0] = X0[1, :]
-    p[0] = X0[2, :]
-    splitting_points = []
+        N = 50
+        Time = np.linspace(0, 1, num=N)
+        dt = Time[1] - Time[0]
+        Time = np.flip(Time)
 
-    start_time = time.time()
+        d = np.zeros((N,))
+        v = np.zeros((N,))
+        u1 = np.zeros((N,))
+        u2 = np.zeros((N,))
+        p = np.zeros((N,))
+        V = np.zeros((N,))
 
-    for j in range(1, Time.shape[0] + 1):
-        X_nn = np.array([[d[j - 1]], [v[j - 1]], [p[j - 1]]])
-        t_nn = np.array([[Time[j - 1]]])
-        _, _, V[j - 1] = value_action(X_nn, t_nn, model)
+        d[0] = X0[0, :]
+        v[0] = X0[1, :]
+        p[0] = X0[2, :]
+        splitting_points = []
 
-        u1[j - 1], u2[j - 1], p_t, sp = optimization(X_nn, t_nn, dt, model)
-        if j == Time.shape[0]:
-            break
-        else:
-            d[j], v[j] = dynamic(X_nn, dt, (u1[j - 1], u2[j - 1]))
-            p[j] = p_t
-            splitting_points.append(sp)
-        print(j)
+        start_time = time.time()
 
-    print()
-    time_spend = time.time() - start_time
-    print('Total solution time: %1.1f' % (time_spend), 'sec')
-    print()
+        for j in range(1, Time.shape[0] + 1):
+            X_nn = np.array([[d[j - 1]], [v[j - 1]], [p[j - 1]]])
+            t_nn = np.array([[Time[j - 1]]])
+            _, _, V[j - 1] = value_action(X_nn, t_nn, model)
 
-    data = {'d': d,
-            'v': v,
-            'u1': u1,
-            'u2': u2,
-            'p': p,
-            'V': V,
-            't': np.flip(Time)}
+            u1[j - 1], u2[j - 1], p_t, sp = optimization(X_nn, t_nn, dt, model, type_i)
+            if j == Time.shape[0]:
+                break
+            else:
+                d[j], v[j] = dynamic(X_nn, dt, (u1[j - 1], u2[j - 1]))
+                p[j] = p_t
+                splitting_points.append(sp)
+            print(j)
 
-    save_data = 1  # input('Save data? Enter 0 for no, 1 for yes:')
+        print()
+        time_spend = time.time() - start_time
+        print('Total solution time: %1.1f' % (time_spend), 'sec')
+        print()
 
-    if save_data:
-        save_path = 'relative_0.2.mat'
-        scio.savemat(save_path, data)
+        data = {'d': d,
+                'v': v,
+                'u1': u1,
+                'u2': u2,
+                'p': p,
+                'V': V,
+                't': np.flip(Time),
+                'type': type_i,
+                'p_0': p_0}
+
+        save_data = 1  # input('Save data? Enter 0 for no, 1 for yes:')
+
+        if save_data:
+            save_path = f'relative_random_{i}.mat'
+            scio.savemat(save_path, data)
+
+    # For testing remove later.
+    # x_plot = torch.zeros(10, 3).uniform_(-1, 1)
+    # x_plot[:, 0] = 0.5 * torch.ones_like(x_plot[:, 0])
+    # time = torch.zeros(10, 1)# final time
+    # coords = torch.cat((time, x_plot), dim=1)
+    # coords_in = {'coords': coords}
+    # vals = model(coords_in)['model_out'].detach().cpu().numpy().squeeze()
+    #
+    # p = x_plot[:, -1].detach().numpy().squeeze()
+    #
+    # plt.plot(p, vals, 'r-')
+    # plt.xlabel('p')
+    # plt.ylabel('V')
+    # plt.title('Value at final time for a fixed state')
+    # plt.show()
