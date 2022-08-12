3660546dd5da16af2f55f2f1a773150d6e68f63c
ghimiremukesh <ghimiremukesh1@gmail.com>
2022-08-11 23:22:54 -0400

    plot multiple trajectories for debugging

--- validation_scripts/soccer_trajectory_rs.py
+++ validation_scripts/soccer_trajectory_rs.py
@@ -26,48 +26,56 @@ def get_abs_states(u1, u2):
     x_p = np.asarray(x_p)
 
     return x_e, x_p
-
-file = 'relative_0.0.mat'
-data = scipy.io.loadmat(file)
-
-d = data['d'].squeeze()
-v = data['v'].squeeze()
-u1 = data['u1'].squeeze()
-u2 = data['u2'].squeeze()
-p = data['p'].squeeze()
-V = data['V'].squeeze()
-t = data['t'].squeeze()
-
-
-d1, d2 = get_abs_states(u1, u2)
-
-d1 = d1[:, 0]
-d2 = d2[:, 0]
 fig, ax = plt.subplots(nrows=5, ncols=1, sharex=True)
-ax[0].plot(t, d1)
-ax[0].set_ylabel('Attacker')
-ax[1].plot(t, d2)
-ax[1].set_ylabel('Defender')
-ax[2].plot(t, p)
-ax[2].set_ylabel('Belief')
-ax[3].plot(t, u1)
-ax[3].set_ylabel('$u_A$')
-ax[4].plot(t, u2)
-ax[4].set_ylabel('$u_D$')
-ax[4].set_xlabel('Time')
+count_1 = 0
+count_2 = 0
+for i in range(10):
+    file = f'relative_random_{i}.mat'
+    data = scipy.io.loadmat(file)
 
-# fig2, ax2= plt.subplots(1, 1)
-# ax2.plot(d1, d2)
+    d = data['d'].squeeze()
+    v = data['v'].squeeze()
+    u1 = data['u1'].squeeze()
+    u2 = data['u2'].squeeze()
+    p = data['p'].squeeze()
+    V = data['V'].squeeze()
+    t = data['t'].squeeze()
+    type = data['type'].squeeze()
+    p_0 = data['p_0'].squeeze()
+    # if (type == 1 and count_1 == 0) or (type == 0 and count_2 == 0):
+    #     count_1 = 1 if type == 1 else count_1
+    #     count_2 = 1 if type == 0 else count_2
 
-fig3, ax3 = plt.subplots(1, 1)
-ax3.plot(t, V, label="NN Value")
-ax3.set_ylabel('Value')
-ax3.set_xlabel('TIme')
+    print(type)
+    print(data['p_0'].squeeze())
+    d1, d2 = get_abs_states(u1, u2)
 
-fig4, ax4 = plt.subplots(1, 1)
-ax4.plot(t, p)
-ax4.set_ylabel('Belief')
-ax4.set_xlabel('Time')
+    d1 = d1[:, 0]
+    d2 = d2[:, 0]
+    ax[0].plot(t, d1, label=f'Type: {type}')
+    ax[0].set_ylabel('Attacker')
+    ax[0].legend(loc='upper right')
+    ax[1].plot(t, d2)
+    ax[1].set_ylabel('Defender')
+    ax[2].plot(t, p)
+    ax[2].set_ylabel('Belief')
+    ax[3].plot(t, u1)
+    ax[3].set_ylabel('$u_A$')
+    ax[4].plot(t, u2)
+    ax[4].set_ylabel('$u_D$')
+    ax[4].set_xlabel('Time')
 
+    # fig2, ax2= plt.subplots(1, 1)
+    # ax2.plot(d1, d2)
+
+    # fig3, ax3 = plt.subplots(1, 1)
+    # ax3.plot(t, V, label="NN Value")
+    # ax3.set_ylabel('Value')
+    # ax3.set_xlabel('TIme')
+    #
+    # fig4, ax4 = plt.subplots(1, 1)
+    # ax4.plot(t, p)
+    # ax4.set_ylabel('Belief')
+    # ax4.set_xlabel('Time')
 plt.show()
 
